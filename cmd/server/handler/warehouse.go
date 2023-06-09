6801b4b1ab19344728d857c05754e944b46dc9ae
Rafael Bonin <rafalyf11@gmail.com>
2023-06-09 17:33:14 -0300

    feaT: warehouse base handler

--- cmd/server/handler/warehouse.go
+++ cmd/server/handler/warehouse.go
@@ -1,21 +1,27 @@
 package handler
 
 import (
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/warehouse"
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
 type Warehouse struct {
-	// warehouseService warehouse.Service
+	warehouseService warehouse.Service
 }
 
-func NewWarehouse() *Warehouse {
+func NewWarehouse(s warehouse.Service) *Warehouse {
 	return &Warehouse{
-		// warehouseService: w,
+		warehouseService: s,
 	}
 }
 
 func (w *Warehouse) Get() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+
+	}
 }
 
 func (w *Warehouse) GetAll() gin.HandlerFunc {
@@ -23,7 +29,20 @@ func (w *Warehouse) GetAll() gin.HandlerFunc {
 }
 
 func (w *Warehouse) Create() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var req domain.WarehouseRequestDTO
+		if err := c.ShouldBindJSON(&req); err != nil {
+			web.Response(c, 400, nil)
+			return
+		}
+
+		result, e := w.warehouseService.Create(req)
+		if e != nil {
+			web.Error(c, 400, e.Error())
+		}
+
+		web.Success(c, 201, result)
+	}
 }
 
 func (w *Warehouse) Update() gin.HandlerFunc {
