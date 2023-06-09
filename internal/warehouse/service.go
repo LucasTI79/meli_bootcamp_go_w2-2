381405dc46f99cf92a13533aac5e6dd4b9ac4736
Rafael Bonin <rafalyf11@gmail.com>
2023-06-09 17:34:17 -0300

    feat: basic create warehouse service

--- internal/warehouse/service.go
+++ internal/warehouse/service.go
@@ -1,16 +1,30 @@
 package warehouse
 
-import "errors"
+import (
+	"context"
+	"errors"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/domain"
+)
 
 // Errors
 var (
 	ErrNotFound = errors.New("warehouse not found")
 )
 
-type Service interface{}
+type Service interface{
+	Create(dto domain.WarehouseRequestDTO) (domain.Warehouse, error)
+}
 
-type service struct{}
+type service struct{
+	repository 
+}
 
-func NewService() Service {
+func (s *service) Create(c context.Context,  dto domain.Warehouse) (domain.Warehouse, error) {
+	result, err := s.repository.Save(c, dto)
+	return result, err
+}
+
+func NewService(r Repository) Service {
 	return &service{}
 }
