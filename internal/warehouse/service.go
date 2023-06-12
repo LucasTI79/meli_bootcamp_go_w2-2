d0e93183e2f4995ab4a869fbecc93dbe524a1cd2
Rafael Bonin <rafalyf11@gmail.com>
2023-06-12 17:11:49 -0300

    feat: update service done

--- internal/warehouse/service.go
+++ internal/warehouse/service.go
@@ -17,6 +17,7 @@ type Service interface{
 	Create(c context.Context, dto dtos.WarehouseRequestDTO) (*domain.Warehouse, error)
 	GetAll(c context.Context) (*[]domain.Warehouse, error)
 	GetOne(c context.Context, id int) (*domain.Warehouse, error)
+	Update(c context.Context, id int, dto dtos.WarehouseRequestDTO) (*domain.Warehouse, error)
 }
 
 type service struct{
@@ -29,7 +30,14 @@ func (s *service) Create(c context.Context, dto dtos.WarehouseRequestDTO) (*doma
 		return nil, errors.New("a warehouse with this warehouse_code already exists")
 	}
 
-	var formatter domain.Warehouse = domain.Warehouse{ID: 0, Address: dto.Address, Telephone: dto.Telephone, WarehouseCode: dto.WarehouseCode, MinimumCapacity: dto.MinimumCapacity, MinimumTemperature: dto.MinimumTemperature}
+	var formatter domain.Warehouse = domain.Warehouse{ 
+		ID: 0,
+		Address: dto.Address,
+		Telephone: dto.Telephone,
+		WarehouseCode: dto.WarehouseCode,
+		MinimumCapacity: dto.MinimumCapacity,
+		MinimumTemperature: dto.MinimumTemperature,
+	}
 
 	id, err := s.repository.Save(c, formatter)
 
@@ -65,7 +73,44 @@ func (s *service) GetOne(c context.Context, id int) (*domain.Warehouse, error) {
 	return &result, nil
 }
 
+func (s *service) Update(c context.Context, id int, dto dtos.WarehouseRequestDTO) (*domain.Warehouse, error) {
+	newWarehouse, er := s.GetOne(c, id)
+
+	if er != nil {
+		return nil, ErrNotFound
+	}
+
+	newWarehouse = updateFormatter(dto, *newWarehouse)	
+
+	err := s.repository.Update(c, *newWarehouse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newWarehouse, nil
+}
 
 func NewService(r Repository) Service {
 	return &service{ repository: r }
+}
+
+func updateFormatter(dto dtos.WarehouseRequestDTO, newWarehouse domain.Warehouse) *domain.Warehouse {
+	if dto.Address != "" {
+		newWarehouse.Address = dto.Address
+	}
+	if dto.Telephone != "" {
+		newWarehouse.Telephone = dto.Telephone
+	}
+	if dto.WarehouseCode != "" {
+		newWarehouse.WarehouseCode = dto.WarehouseCode
+	}
+	if dto.MinimumCapacity != 0 {
+		newWarehouse.MinimumCapacity = dto.MinimumCapacity
+	}
+	if dto.MinimumTemperature != 0 {
+		newWarehouse.MinimumTemperature = dto.MinimumTemperature
+	}
+
+	return &newWarehouse
 }
