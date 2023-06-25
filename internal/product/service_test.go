5ca074e6884b7858493904f68afe4d4afb24398d
maicodsantos <maicon.dsantos@mercadolivre.com>
2023-06-25 15:13:32 -0300

    Adding Get Products Service Tests.

---
+++ internal/product/service_test.go
@@ -0,0 +1,74 @@
+package product_test
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/product"
+	"github.com/extmatperez/meli_bootcamp_go_w2-2/internal/product/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGet(t *testing.T) {
+
+	t.Run("find_by_id_existent", func(t *testing.T) {
+		expectedProduct := &domain.Product{
+			ID:             1,
+			Description:    "Test",
+			ExpirationRate: 1,
+			FreezingRate:   1,
+			Height:         1.1,
+			Length:         1.1,
+			Netweight:      1.1,
+			ProductCode:    "Test",
+			RecomFreezTemp: 1.1,
+			Width:          1.1,
+			ProductTypeID:  1,
+			SellerID:       1,
+		}
+
+		ctx := context.TODO()
+
+		productRepositoryMock := new(mocks.ProductRepositoryMock)
+		productRepositoryMock.On("Get", ctx, mock.AnythingOfType("int")).Return(*expectedProduct, nil)
+
+		service := product.NewService(productRepositoryMock)
+		productReceived, err := service.Get(&ctx, 1)
+
+		assert.Equal(t, *expectedProduct, *productReceived)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("find_by_id_non_existent", func(t *testing.T) {
+
+		ctx := context.TODO()
+
+		productRepositoryMock := new(mocks.ProductRepositoryMock)
+		productRepositoryMock.On("Get", ctx, mock.AnythingOfType("int")).Return(domain.Product{}, sql.ErrNoRows)
+
+		service := product.NewService(productRepositoryMock)
+		productReceived, err := service.Get(&ctx, 1)
+
+		assert.Nil(t, productReceived)
+		assert.Equal(t, product.ErrNotFound, err)
+	})
+
+	t.Run("unexpected_error", func(t *testing.T) {
+
+		ctx := context.TODO()
+
+		productRepositoryMock := new(mocks.ProductRepositoryMock)
+		productRepositoryMock.On("Get", ctx, mock.AnythingOfType("int")).Return(domain.Product{}, errors.New("error"))
+
+		service := product.NewService(productRepositoryMock)
+		productReceived, err := service.Get(&ctx, 1)
+
+		assert.Nil(t, productReceived)
+		assert.Equal(t, errors.New("error"), err)
+	})
+
+}
