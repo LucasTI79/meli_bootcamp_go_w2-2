58845416ea4f149cb4327e98155298468f1d8db2
dudameregali <melmeregali@gmail.com>
2023-07-23 17:06:04 -0300

    inclusão testes repository inbound orders e ajustes na service e repository

--- internal/domain/inbound_orders.go
+++ internal/domain/inbound_orders.go
@@ -4,7 +4,7 @@ type InboundOrders struct {
 	ID             int    `json:"id"`
 	OrderDate      string `json:"order_date"`
 	OrderNumber    string `json:"order_number"`
-	EmployeeID     string `json:"lemployee_id"`
+	EmployeeID     string `json:"employee_id"`
 	ProductBatchID string `json:"product_batch_id"`
 	WarehouseID    string `json:"warehouse_id"`
 }
@@ -12,7 +12,7 @@ type InboundOrders struct {
 type RequestCreateInboundOrders struct {
 	OrderDate      string `json:"order_date"`
 	OrderNumber    string `json:"order_number"`
-	EmployeeID     string `json:"lemployee_id"`
+	EmployeeID     string `json:"employee_id"`
 	ProductBatchID string `json:"product_batch_id"`
 	WarehouseID    string `json:"warehouse_id"`
 }
@@ -20,7 +20,7 @@ type RequestCreateInboundOrders struct {
 type RequestUpdateInboundOrders struct {
 	OrderDate      *string `json:"order_date"`
 	OrderNumber    *string `json:"order_number"`
-	EmployeeID     *string `json:"lemployee_id"`
+	EmployeeID     *string `json:"employee_id"`
 	ProductBatchID *string `json:"product_batch_id"`
 	WarehouseID    *string `json:"warehouse_id"`
 }
